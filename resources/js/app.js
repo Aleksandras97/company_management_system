716ced02bb96648bb4d67eaedde35b55ee2518af
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-07-29 00:56:55 +0300

    Project init with vue 3 and prime vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+
+import App from './vue/App.vue';
+
+import {createApp} from 'vue';
+
+
+const app = createApp({});
+app.component('App', App);
+app.mount('#app')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Comany Management System</title>
+    <title>Comany Management System</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/app.css"/>
 
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body id="app">
-
-    </body>
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+<body id="app">
+    <App></App>
+</body>
+<script src="{{ mix('/js/app.js') }}"></script>
 </html>
