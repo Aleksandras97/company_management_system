f9fac5afb271bb005cc58a9db7ca5f777fb4f7c7
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2021-08-01 18:31:21 +0300

    Add modal and Toast

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,12 @@ import InputText from 'primevue/inputtext';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Dialog from 'primevue/dialog';
+import ConfirmDialog from 'primevue/confirmdialog';
+import Toast from 'primevue/toast';
+
+
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
 
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                //core css
@@ -16,10 +22,14 @@ import 'primeicons/primeicons.css'                         //icons
 
 const app = createApp({});
 app.use(PrimeVue);
+app.use(ConfirmationService);
+app.use(ToastService);
 app.component('App', App);
 app.component('Button', Button);
 app.component('InputText', InputText);
 app.component('DataTable', DataTable);
 app.component('Column', Column);
 app.component('Dialog', Dialog);
+app.component('ConfirmDialog', ConfirmDialog);
+app.component('Toast', Toast);
 app.mount('#app')
